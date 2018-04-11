19c152667c56d1e72eb630e77a703903798b3828
Phillip Oh <ohphillipj@gmail.com>
2018-04-11 15:41:02 -0700

    Add variance plots, further data cleaning

--- data_formatter.py
+++ data_formatter.py
@@ -5,7 +5,7 @@ import data_utils
 
 wait_time = 60
 seq_len = 60
-num_windows = 5
+num_windows = 6
 activity2id = {"Walking": 0, "Jogging": 1, "Stairs": 2, "Sitting": 3, "Standing": 4, "LyingDown": 5}
 
 with open('D:\Documents\AML\RGAN\experiments\data\WISDM_at_v2.0_raw.txt', 'rt') as csvfile:
@@ -17,7 +17,7 @@ with open('D:\Documents\AML\RGAN\experiments\data\WISDM_at_v2.0_raw.txt', 'rt') 
 
 cur_id = 0
 cur_count = 0
-num_windows = 1
+cur_windows = 1
 
 labels = []
 samples = []
@@ -33,24 +33,28 @@ for entry in raw_data:
         labels.append(activity2id[entry[1]])
         samples.append([[], [], []])
         cur_count = 1
-        num_windows = 1
+        cur_windows = 1
     else:
-        if num_windows > 5:
+        if cur_windows > num_windows:
             continue
         if cur_count >= seq_len:
             labels.append(activity2id[entry[1]])
             samples.append([[], [], []])
             cur_count = 0
-            num_windows += 1
+            cur_windows += 1
         cur_count += 1
         # if cur_count <= wait_time+1:
         #     continue
-    samples[-1][0].append(np.float(entry[3]))
-    samples[-1][1].append(np.float(entry[4]))
-    samples[-1][2].append(np.float(entry[5][:-1]))
+    if cur_windows > 2:         # Wait 2 windows before sampling
+        samples[-1][0].append(np.float(entry[3]))
+        samples[-1][1].append(np.float(entry[4]))
+        samples[-1][2].append(np.float(entry[5][:-1]))
 
+# Remove standing data that has too high variance
 mask = []
-vars = []
+stand_vars = [[],[],[]]
+walk_vars = [[],[],[]]
+jog_vars = [[],[],[]]
 for data_point in range(len(samples)):
     samples[data_point] = np.array(samples[data_point])
 
@@ -58,15 +62,43 @@ for data_point in range(len(samples)):
         mask.append(data_point)
 
     elif labels[data_point] == 4:
-        vars.append(np.var(samples[data_point]))
+        for i in range(0,3):
+            stand_vars[i].append(np.var(samples[data_point][i,:]))
         if np.var(samples[data_point]) > 0.5:
             mask.append(data_point)
 
+    elif labels[data_point] == 0:
+        for i in range(0,3):
+            walk_vars[i].append(np.var(samples[data_point][i,:]))
+
+    elif labels[data_point] == 1:
+        for i in range(0,3):
+            jog_vars[i].append(np.var(samples[data_point][i,:]))
+
+# Remove running data that has too low variance
+
+
 # Plot variance
-# n, bins, patches = plt.hist(np.array(vars), 20, normed=1, facecolor='green', alpha=0.75)
-#
-# print(np.mean(vars))
-# plt.show()
+plt.figure(1)
+plt.subplot(311)
+plt.hist(np.array(stand_vars[0]), 20, normed=1, facecolor='green', alpha=0.75, label='standx')
+plt.hist(np.array(stand_vars[1]), 20, normed=1, facecolor='blue', alpha=0.75, label='standy')
+plt.hist(np.array(stand_vars[2]), 20, normed=1, facecolor='red', alpha=0.75, label='standz')
+plt.title("Standing variance")
+plt.subplot(312)
+plt.hist(np.array(walk_vars[0]), 20, normed=1, facecolor='green', alpha=0.75, label='walkx')
+plt.hist(np.array(walk_vars[1]), 20, normed=1, facecolor='blue', alpha=0.75, label='walky')
+plt.hist(np.array(walk_vars[2]), 20, normed=1, facecolor='red', alpha=0.75, label='walkz')
+plt.title("Walking variance")
+plt.subplot(313)
+plt.hist(np.array(jog_vars[0]), 20, normed=1, facecolor='green', alpha=0.75, label='jogx')
+plt.hist(np.array(jog_vars[1]), 20, normed=1, facecolor='blue', alpha=0.75, label='jogy')
+plt.hist(np.array(jog_vars[2]), 20, normed=1, facecolor='red', alpha=0.75, label='jogz')
+plt.title("Jogging variance")
+plt.show()
+print("Walking variance:", np.mean(walk_vars))
+print("Jogging variance:", np.mean(jog_vars))
+plt.show()
 
 for i in sorted(mask, reverse=True):
     del samples[i]
